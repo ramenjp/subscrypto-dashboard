0bc1b603e1e5f5f7409d5bc5e141f62093b5e4cc
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-08-23 18:09:17 +0900

    success page

---
+++ pages/success.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import type { NextPage } from "next";
+import { Success as SuccessTemplate } from "../components/template/success";
+const Success: NextPage = () => {
+  return <SuccessTemplate subscryptoAddress="" />;
+};
+export default Success;
